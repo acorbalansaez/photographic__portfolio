c01afeaf90afe31983eeed51334fe515608ef256
acorbalansaez <agustina.lara.cs@gmail.com>
2023-07-11 01:06:40 -0300

    sin bootstrap

--- aboutme.js
+++ aboutme.js
@@ -1,2 +1,23 @@
 gsap.registerPlugin(ScrollTrigger);
 
+const aboutMeContainer = document.querySelector(".aboutMeContainer");
+
+
+const timelineScroll = gsap.timeline({
+
+    scrollTrigger: {
+        // pin: "body",
+        scrub: 2,
+        // markers: true,
+        trigger: aboutMeContainer,
+        end: 2500, // nro estimativo a re-ver
+        //end: galleryContentContainer.scrollWidth - document.documentElement.clientWidth + galleryContentContainer.offsetWidth,
+        // yoyo: false,
+        // toggleActions: "none none none none",
+    }
+})
+
+
+function printScrollPosition() {
+    console.log("Posición del scroll:", window.scrollY);
+}
