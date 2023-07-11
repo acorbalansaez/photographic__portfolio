419b2968ef46049c48685d5d7e18774c47db1f0c
acorbalansaez <agustina.lara.cs@gmail.com>
2023-07-11 19:13:24 -0300

    desplazamiento del bigContainer y su ancho solucionado

--- aboutme.js
+++ aboutme.js
@@ -30,10 +30,6 @@ function createScrollTrigger() {
             pin: "body",
             scrub: 2,
             trigger: bigContainer,
-            //end: 2500, // nro estimativo
-            //end: aboutMeSection1.scrollWidth - document.documentElement.clientWidth + aboutMeSection1.offsetWidth,
-            //end: getEndValue,
-            markers: true,
         }
     })
 
@@ -43,38 +39,32 @@ function createScrollTrigger() {
 
 function enableScrollTrigger() {
 
-    //let aboutMeMainContent__img02 = document.querySelector('.aboutMeMainContent__img02');
-    //const viewportHeight = window.innerHeight;
-    //let imgAboutMe02Width = viewportHeight * (aboutMeMainContent__img02.naturalWidth / aboutMeMainContent__img02.naturalHeight);
 
     if (!timelineScroll) {
         timelineScroll = createScrollTrigger();
     }
 
-    //const containerWidth = bigContainer.offsetWidth - window.innerWidth;
-    //timelineScroll.to(bigContainer, { x: -containerWidth, duration: 1.8 });
-
+    // el bigContainer se desplaza en su ancho
     console.log(bigContainer.offsetWidth - window.innerWidth);
     timelineScroll.to(bigContainer, { x: () => -(bigContainer.offsetWidth - window.innerWidth), duration: 1.8 })
-    // timelineScroll.to(bigContainer, { xPercent: "-100", duration: 1.8 }, 0)
     .fromTo(assetPhotographer, { rotation: 30, right: '34vw' }, { rotation: -80, right: '50vw', ease: 'power2', duration: 1.5 }, 0)
 
 
     let tlScroll2 = gsap.timeline({
         scrollTrigger: {
-            trigger: ".aboutMeMainContent__img02",
+            trigger: ".aboutMeMainContent__img01",
             start: "right right",
-            //end: "left left",
             end: 2000,
             scrub: 2,
+            markers: true,
         }
     })
 
     tlScroll2.to(".aboutMeMainContent__img02", { top: '0vh', ease: 'power2', duration: 0.01 }, 0)
         .to(".aboutMeMainContent__img01", { top: '10vh', x: '-3vw', ease: 'power2', duration: 0.1 }, "<")
-        .fromTo(".aboutMeSection2", { autoAlpha: 0 }, { autoAlpha: 1, ease: 'power2', duration: 0.1 }, "<")
-        .fromTo(".aboutMeSection3__assetAgs", { rotation: -60 }, { rotation: 60, ease: 'power2', duration: 0.6 }, "<")
-        .fromTo(".aboutMeSection3__assetWow", { rotation: 60 }, { rotation: -60, ease: 'power2', duration: 0.6 }, "<")
+        .fromTo(".aboutMeSection2", { autoAlpha: 0 }, { autoAlpha: 1, ease: 'power1', duration: 0.0 }, 0.001)
+        .fromTo(".aboutMeSection3__assetAgs", { rotation: 0 }, { rotation: 360, ease: 'power2', duration: 0.1 }, "<")
+        .fromTo(".aboutMeSection3__assetWow", { rotation: 60 }, { rotation: 60, ease: 'power2', duration: 0.01 }, "<")
         .fromTo(".aboutMeSection3__img01", { y: '-20vh' }, { y: 0, ease: 'power2', duration: 0.4 }, "<")
         .to(".aboutMeSection3__assetAgs", { y: '22vh', ease: 'power2', duration: 0.1 }, "<+0.1")
         .to(".aboutMeSection3__assetWow", { y: '22vh', ease: 'power2', duration: 0.1 }, "<")
