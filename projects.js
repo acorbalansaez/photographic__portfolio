fca50d698126893e6785d7336589184bf81e59d3
acorbalansaez <agustina.lara.cs@gmail.com>
2023-08-08 19:26:11 -0300

    projects mobile avances

--- projects.js
+++ projects.js
@@ -1,12 +1,16 @@
 const canvas = document.querySelector(".projectsCanvas");
 const contenedor = document.querySelector(".projectsContent");
 
+const navbarAgs = document.getElementById('ags');
+
 import { mouseCursor, imagesProjects } from './cursor.js';
 
 Draggable.create(".projectsCanvas", {
 
     zIndexBoost: false, // para que no agregue z-index 1000 por defecto
     bounds: contenedor, // setea el límite hasta donde se puede mover
+    edgeResistance: 0.65,
+
 
     onDrag: function () {
 
@@ -22,13 +26,29 @@ Draggable.create(".projectsCanvas", {
             img.style.transform = 'scale(0.92)';
         });
 
+        // cambia cursor
+        mouseCursor.classList.remove('cursor-projects');
+        mouseCursor.classList.add('cursor-projects-little');
+
     },
 
     onDragEnd: function () {
+
         imagesProjects.forEach(imgOnHover => {
             const img = imgOnHover.querySelector('.img');
             img.style.transform = 'scale(1)';
+
         });
-    }
+
+        // cambia cursor
+        mouseCursor.classList.remove('cursor-projects-little');
+        mouseCursor.classList.add('cursor-projects');
+    },
 
 });
+
+const screenWidth = document.documentElement.clientWidth;
+
+if (screenWidth <= 768) {
+    navbarAgs.remove();
+}
